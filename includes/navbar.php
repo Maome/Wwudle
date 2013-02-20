2abcc5eb1ea93ca5625812ac64391f9f2e47b8c6
Nick Tarter <ar2server@gmail.com>
2013-02-20 11:32:08 -0800

    Renamed test.php to index.php Modified sidebar and navbar to render from include files

---
+++ includes/navbar.php
@@ -0,0 +1,44 @@
+ <?php
+	function DisplayNavbar($filename) {
+	echo '
+		<div class="navbar navbar-fixed-top">
+			<div class="navbar-inner">
+				<div class="container">
+					<a class="btn btn-navbar" data-toggle="collapse" data-target=".nav-responsive-collapse">
+						<span class="icon-bar"></span>
+						<span class="icon-bar"></span>
+						<span class="icon-bar"></span>
+					</a>
+					<a class="brand" href="#">Western List</a>
+					<div class="nav-collapse collapse navbar-responsive-collapse">
+						<ul class="nav">
+							' .li_type($filename,"index.php") .'<a href="index.php">Home</a></li>
+							<li class="dropdown">
+								<a class="dropdown-toggle" href="" role="button" data-toggle="dropdown">Reviews<b class="caret"></b></a>
+								<ul class="dropdown-menu" role="menu" aria-labelledby="dropl">
+									' .li_type($filename,"courses.php") .'<a tabindex="-1" href="courses.php">Courses</a></li>
+									' .li_type($filename,"textbooks.php") .'<a tabindex="-1" href="textbooks.php">Textbooks</a></li>
+									' .li_type($filename,"professors.php") .'<a tabindex="-1" href="professors.php">Professors</a></li>
+								</ul>
+							</li>
+							' .li_type($filename,"rideshare.php") .'<a href="rideshare.php">Rides</a></li>
+							' .li_type($filename,"buysell.php") .'<a href="buysell.php">Buy / Sell</a></li>
+						</ul>
+						
+						<p class="navbar-text pull-right">Logged in as <a href="#" class="navbar-link">Username</a></p>
+						<ul class="nav pull-right"><li class="divider-vertical"></li></ul>
+						<form class="navbar-search pull-right">
+							<input type="text" class="navbar-search search-query" placeholder="search">
+						</form>
+					</div><!--/.nav-collapse -->
+				</div>
+			</div>
+		</div>
+	';
+	}
+
+	function li_type($filename,$expected_filename) {
+		if ($filename == $expected_filename) return '<li class="active">';
+		else return '<li>';
+	}
+?>
---
+++ includes/sidebar.php
@@ -0,0 +1,25 @@
+<?php
+	function DisplaySidebar() {
+		require_once('../../private_html/connect.php');
+		echo '
+		<div class="span3">
+				<ul class="nav nav-tabs nav-stacked">
+					<li class="active"><a href="">Western Links</a></li>
+		';
+			// Display each navbar link
+			$connection->real_query("SELECT URL, Text FROM CS492_201310.HyperLink WHERE LinkType = 1 AND RecordStatus <> 3 ORDER BY Position;");
+			$field_count = $connection->field_count;
+
+				if ($field_count) {
+						 $result = $connection->store_result();
+
+						while ($row = $result->fetch_row()) {
+									echo '<li><a href="' .$row[0] .'">' .$row[1] ."</a></li>";
+						}
+				}
+		echo '
+				</ul>
+		</div>
+		';
+	}
+?>
