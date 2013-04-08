3d11d719d9564ec11d746a237dd827931f988512
Kyle William Hiebert <hieberk@quest-test>
2013-04-08 11:27:45 -0700

    added request to rideshare

---
+++ ridesharerequest.php
@@ -0,0 +1,84 @@
+<?php
+	require_once('cassettings.php');
+	require_once('includes/navbar.php');
+	require_once('includes/sidebar.php');
+	include_once("../../private_html/connect.php");
+?>
+<!DOCTYPE HTML>
+<html lang-"en">
+    <head>
+        <title>Western List</title>
+        <link href="bootstrap/css/bootstrap.css" rel="stylesheet">
+        <link href="bootstrap/css/bootstrap-responsive.css" rel="stylesheet">
+    </head>
+    <body>
+
+		<!-- Navbar -->
+		<?php DisplayNavbar(basename("rideshare.php")); ?>
+        
+        <div class="container">
+            <div class="row-fluid">
+				<!-- Sidebar -->
+				<?php DisplaySidebar(); ?>
+                <div class="span9">
+                    <div class="row-fluid">
+                        <div class="span6">
+
+                            <!-- MAIN CONTENT FOR RIDEINFO -->
+							<?php
+								$dbc = new dbw(DBSERVER,DBUSER,DBPASS,DBCATALOG);
+								
+								// Check to see if user is logged on/Valid user
+								$username = PHPCAS::GetUser();
+								
+								// See if the user is in the Users table
+								$username = $username . "@students.wwu.edu";
+								$sql = "Select * from User where Email = '$username';";
+								
+								$result = $dbc->query($sql);
+																
+								// Check if the name is in the table
+								$rows = $result->num_rows;					
+								
+								if ($rows == 0) {
+									echo "<h2>You must log in!</h2>";
+								}
+								else{															
+									// Get the post data
+									$PostID = $_GET['PostID'];
+									
+									// Get the email for the person who posted the ride
+									//$sql = "SELECT Email FROM User WHERE UserID = '$PostID';";
+									//$result = $dbc->query($sql);
+									//$row = $result->fetch_row();
+									//$PostEmail = $row[0];
+									
+									$sql = "SELECT SourceCity, DestCity FROM RideShare WHERE PostID = '$PostID';";
+									$result = $dbc->query($sql);
+									$row = $result->fetch_row();
+									$Source = $row[0];
+									$Dest = $row[1];
+									
+									// Create a form to get the message
+									echo "
+			                            <form class='form-inline' action='sendridesharerequest.php' method='post'>
+											<input type='text'>Rideshare request from $Source to $Dest</input> <br /><br />
+			                            	<textarea class='field span12' rows='10' placeholder='Type your message here' name='MessageBody'></textarea><br /><br />
+			                                <button type='submit' class='btn btn-primary'>Request Ride</button>
+			                            </form>																				
+									";
+								}																   																											
+							?>
+							
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+        
+    </body>
+    <script src="holder/holder.js"></script>
+    <script src="http://code.jquery.com/jquery-latest.js"></script>
+    <script src="bootstrap/js/bootstrap.js"></script>
+</html>
+
