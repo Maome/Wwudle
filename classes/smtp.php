9667eada513bafe9901d50c4a4f0bfc0f07259c5
Kyle William Hiebert <hieberk@quest-test>
2013-06-05 11:13:32 -0700

    small fixes

--- classes/smtp.php
+++ classes/smtp.php
@@ -3,8 +3,8 @@
 	
 	//Server Address
 
-		$SmtpServer="127.0.0.1";
-		$SmtpPort="25"; //default
+		//$SmtpServer="127.0.0.1";
+		//$SmtpPort="25"; //default
 		//$SmtpUser="username";
 		//$SmtpPass="password";
 	
